46d9dd9d45dee9c925c6a60c257fe3da4de09f75
Indrapalsingh8241 <thakurindrapal09@gmail.com>
2026-05-19 12:04:22 +0530

    Fixed LangChain imports

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
+from langchain_core.prompts import PromptTemplate
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from langchain_groq import ChatGroq
