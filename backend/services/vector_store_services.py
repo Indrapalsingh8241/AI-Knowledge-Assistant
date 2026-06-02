ce778b3ebe607c86ede547cfb2da8562192b54d9
Indrapalsingh8241 <thakurindrapal09@gmail.com>
2026-06-02 15:38:43 +0530

    Fix LangChain text splitter import

--- backend/services/vector_store_services.py
+++ backend/services/vector_store_services.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 
 
